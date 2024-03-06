9f904f758286b5eba127167fda09f816c03e4f80
ramila <ramila.kl123@gmail.com>
2024-03-06 19:13:06 +0500

    login partially completed

---
+++ frontend/src/component/LoginForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+import {RiWallet2Fill} from 'react-icons/ri'
+
+const LoginForm = () => {
+    return (
+        <div className=''>
+            <div className=' flex align-middle justify-center text-center py-4'>
+                <span className='text-2xl text-purple-700 pe-2'><RiWallet2Fill /></span>
+                <span className=''>PayHabib</span>
+            </div>
+            <div className='my-3 text-center'>
+                <h3 className='p-3 text-3xl font-semibold'>Login</h3>
+                <h3 className='p-3 text-3xl font-semibold'><span className='text-purple-700'>PayHabib</span> account</h3>
+            </div>
+            <Form>
+                <Form.Group className="mb-4" controlId="username">
+                    <Form.Label className='font-bold'>Username</Form.Label>
+                    <Form.Control required className='bg-light' type="text" placeholder="Enter username" />
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="password">
+                    <Form.Label className='font-bold'>Password</Form.Label>
+                    <Form.Control required className='bg-light' type="password" placeholder="Enter at least 8+ characters" />
+                </Form.Group>
+                <Form.Group className="mt-5">
+                    <Form.Control className='bg-purple-700 text-white font-bold text-xl py-2 shadow-xl' type="button" value="Sign up" />
+                </Form.Group>
+            </Form>
+        </div>
+    )
+}
+
+export default LoginForm
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import LoginForm from '../component/LoginForm'
 
 const Login = () => {
     return (
-        <div className='container'>
-            <div className='col-6'>
-                <div className='text-center'>
-                    <div>
-                        <h2>Login</h2>
-                        <h2>PayHabib account</h2>
-                    </div>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="passwordd" placeholder="Enter at least 8+ characters" />
-                        </Form.Group>
-                        <Button size="lg">
-                            Submit
-                        </Button>
-                    </Form>
+        <div className='container mx-auto'>
+            <div className='row'>
+                <div className='col-6 py-24 px-28'>
+                    <LoginForm />
+                </div>
+                <div className='col-6'>
+                    <div className='bg-purple-700 h-full'></div>
                 </div>
             </div>
-            <div className='col-6'></div>
         </div>
     )
 }
