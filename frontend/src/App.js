500dde1a02581fd1b0d0cb80b586b850b3663247
Adnan Ahmed <adnanahmedbilalbn@gmsil.com>
2024-03-06 19:07:12 +0500

    worked on customer dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CustomerDashboard from './pages/CustomerDashboard';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
+        <Route exact path='/customerDashboard' element={<CustomerDashboard />} />
       </Routes>
 
     </BrowserRouter>
