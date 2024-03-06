3b33c1536887ff9e8a5e7b384d2b6050b460f9ab
ramila <ramila.kl123@gmail.com>
2024-03-06 17:29:52 +0500

    Basic Start

---
+++ merchant-portal/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ merchant-portal/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div className='col-6'>
+        </div>
+        <div className='col-6'></div>
+    </div>
+  )
+}
+
+export default Login
